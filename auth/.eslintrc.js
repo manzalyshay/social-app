dc61851ae9d8f7bbe8272341658595077a09be29
Shay Manzaly <shaym@liveperson.com>
2023-12-10 11:11:25 +0200

    feat: add first test

--- auth/.eslintrc.js
+++ auth/.eslintrc.js
@@ -1,8 +1,21 @@
 module.exports = {
-  extends: "airbnb-typescript-prettier",
-  settings: {
-    react: {
-      version: "999.999.999"
-    }
+  extends: [
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint"
+  ],
+  plugins: ["import", "prettier", "@typescript-eslint"],
+  parserOptions: {
+    project: "./tsconfig.json"
+  },
+  rules: {
+    quotes: ["error", "double", { avoidEscape: true, allowTemplateLiterals: true }],
+    "import/prefer-default-export": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "no-underscore-dangle": ["error", { allow: ["_id", "_update"] }],
+    "class-methods-use-this": "off",
+    "@typescript-eslint/no-non-null-assertion": "off"
   }
 };
